124b472f6b3a3e0fc5ba447e335df4ec5a24d9fa
Ryan Lelek <ryanlelek@gmail.com>
2024-06-19 01:48:33 -0500

    Add: DaisyUI for theming

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["winter", "night"],
+    darkTheme: "night",
+  },
 };
 export default config;
